24d8aa14c37b66efba98ff1615371a41048cd081
Everaldo Torres <torresjr1984@hotmail.com>
2022-01-11 11:48:35 -0300

    Projeto de recuperação

---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author torre
+ */
+public class Produto {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the qtde
+     */
+    public int getQtde() {
+        return qtde;
+    }
+
+    /**
+     * @param qtde the qtde to set
+     */
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+    private int id;
+    private String descricao;
+    private double valor;
+    private int qtde;
+}
